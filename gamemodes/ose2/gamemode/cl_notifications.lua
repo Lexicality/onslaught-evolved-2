3c5714672f544db38f80ab74f1c150ed19f83f60
Lexi Robinson <lexi@lexi.org.uk>
2025-02-17 11:18:58 +0000

    Copy in the sandbox hint system
    I'm gonna muck about with this a bunch so I figured it'd be a good idea
    to have a barely edited version in git beforehand.
    
    (Barely edited because the formatter knows no bounds)

--- gamemodes/ose2/gamemode/cl_notifications.lua
+++ gamemodes/ose2/gamemode/cl_notifications.lua
@@ -15,6 +15,8 @@
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 --]]
 
+CreateClientConVar("ose_showhints", "1", true, false, "Whether to display popup hints.")
+
 --- Sandbox Compat
 --- @param str string
 --- @param type number
@@ -71,3 +73,53 @@ function GM:OnCleanup(name)
 
 	surface.PlaySound("buttons/button15.wav")
 end
+
+-- A list of hints we've already done so we don't repeat ourselves`
+local ProcessedHints = {}
+
+--
+-- Throw's a Hint to the screen
+--
+local function ThrowHint(name)
+	local show = GetConVarNumber("cl_showhints")
+	if (show == 0) then return end
+
+	if (engine.IsPlayingDemo()) then return end
+
+	local text = language.GetPhrase("Hint_" .. name)
+
+	local s, e, group = string.find(text, "%%([^%%]+)%%")
+	while (s) do
+		local key = input.LookupBinding(group)
+		if (! key) then key = group:lower() .. " not bound" else key = key:upper() end
+
+		text = string.gsub(text, "%%([^%%]+)%%", "'" .. key .. "'")
+		s, e, group = string.find(text, "%%([^%%]+)%%")
+	end
+
+	GAMEMODE:AddNotify(text, NOTIFY_HINT, 20)
+
+	surface.PlaySound("ambient/water/drip" .. math.random(1, 4) .. ".wav")
+end
+
+
+--
+-- Adds a hint to the queue
+--
+function GM:AddHint(name, delay)
+	if (ProcessedHints[name]) then return end
+
+	timer.Create("HintSystem_" .. name, delay, 1, function() ThrowHint(name) end)
+	ProcessedHints[name] = true
+end
+
+--
+-- Removes a hint from the queue
+--
+function GM:SuppressHint(name)
+	timer.Remove("HintSystem_" .. name)
+end
+
+-- Tell them how to turn the hints off after 1 minute
+-- GM:AddHint( "Annoy1", 5 )
+-- GM:AddHint( "Annoy2", 7 )
