e98e66c0155f11dae5e05e38337c338fd0ffca02
Lexi Robinson <lexi@lexi.org.uk>
2025-03-29 18:13:57 +0000

    Relicence the project to the EUPL
    Literally the only reason I didn't license this as the EUPL to start
    with is I didn't read it properly and didn't realise it has the same
    (well, dramatically better actually) network access clause as the AGPL
    does, meaning it is simply a direct upgrade compared to the GNU nonsense.

--- gamemodes/ose2/gamemode/player_class/player_soldier.lua
+++ gamemodes/ose2/gamemode/player_class/player_soldier.lua
@@ -1,18 +1,12 @@
 --[[
  Copyright (C) 2025 Lexi Robinson
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
+ Licensed under the EUPL, Version 1.2
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ You may not use this work except in compliance with the Licence.
+ You should have received a copy of the Licence along with this work. If not, see:
+ <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
+ See the Licence for the specific language governing permissions and limitations under the Licence.
 --]]
 
 AddCSLuaFile()
